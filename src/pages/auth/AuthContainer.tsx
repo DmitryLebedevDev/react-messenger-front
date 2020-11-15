c923e651fc4402e0df84c5982db6c1ae86c6b35d
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2020-11-16 01:53:33 +0800

    add classnames and refactoring

--- src/pages/auth/AuthContainer.tsx
+++ src/pages/auth/AuthContainer.tsx
@@ -1,21 +1,19 @@
 import React, { FunctionComponent } from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 import { LoginPage } from './LoginPage'
 import { RegistrationPage } from './RegistrationPage'
 import { AuthBg } from './components/AuthBg';
-import { QuickRegistrationPage } from './QuickRegistrationPage';
-import { WhiteWindow } from './components/WhiteWindow';
+import { QuickRegistrationPage } from './QuickRegistrationPage'
+import { WhiteWindow } from './components/WhiteWindow'
 
 export const AuthContainer:FunctionComponent = () => {
   return (
-    <AuthBg>
+    <AuthBg content="center">
       <WhiteWindow>
-        <Switch>
-          <Route path='/login' component={LoginPage}/>
-          <Route path='/quickRegistration' component={QuickRegistrationPage}/>
-          <Route path='/registration' component={RegistrationPage}/>
-        </Switch>
+        <Route path='/login' component={LoginPage}/>
+        <Route path='/quickRegistration' component={QuickRegistrationPage}/>
+        <Route path='/registration' component={RegistrationPage}/>
       </WhiteWindow>
     </AuthBg>
   )
--- src/pages/auth/components/AuthBg.tsx
+++ src/pages/auth/components/AuthBg.tsx
@@ -1,26 +1,38 @@
 import React, { FunctionComponent } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import cn from 'classnames'
 
 import mountainsImage from '../../../images/mountain-3351654_1920.jpg'
+
+interface Iprops {
+  content?: 'center'
+}
 
 const useStyles = makeStyles({
   bgImage: ({urlImage}:{urlImage:string}) => ({
     width: '100%',
     height: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundPosition: 'center',
     backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
     backgroundImage: `url(${urlImage})`
-  })
+  }),
+  contentCenter: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 })
-export const AuthBg:FunctionComponent = ({children}) => {
+
+export const AuthBg:FunctionComponent<Iprops> = ({children, content}) => {
   const classes = useStyles({urlImage: mountainsImage})
+  const wrapClasses = cn([
+    classes.bgImage,
+    {[classes.contentCenter]: Boolean(content)}
+  ])
 
   return (
-    <div className={classes.bgImage}>
+    <div className={wrapClasses}>
       {children}
     </div>
   )
