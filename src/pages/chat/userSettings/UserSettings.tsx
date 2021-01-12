2d8d3daed24860cda6af1db93f7dd733860eb213
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-01-12 23:02:32 +0800

    add gray fon

--- src/pages/chat/userSettings/UserSettings.tsx
+++ src/pages/chat/userSettings/UserSettings.tsx
@@ -3,7 +3,18 @@ import { makeStyles } from '@material-ui/core'
 
 const useStyles = makeStyles({
   wrapSettings: {
-
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  shadow: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: -1,
+    width: '100vw',
+    height: '100vh',
+    background: '#7c7c7c75'
   }
 })
 
@@ -11,8 +22,9 @@ export const UserSettings:FunctionComponent = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.wrapSettings}>
-      settings
-    </div>
+    <>
+      <div className={classes.wrapSettings}>settings</div>
+      <div className={classes.shadow}>1</div>
+    </>
   )
 }
