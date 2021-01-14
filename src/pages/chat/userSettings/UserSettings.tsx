79484caf50a3f63db4cc5622b089022b588f3722
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-01-14 23:15:38 +0800

    add animation on open settings

--- src/pages/chat/userSettings/UserSettings.tsx
+++ src/pages/chat/userSettings/UserSettings.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core'
+import cn from 'classnames'
 
 const useStyles = makeStyles({
-  wrapSettings: {
+  settings: {
     zIndex: 2,
     position: 'absolute',
     top: 0,
@@ -10,6 +11,11 @@ const useStyles = makeStyles({
     width: '320px',
     height: '100vh',
     background: 'var(--background)',
+    transform: 'translateX(-100%)',
+    transition: '.2s',
+  },
+  settingsOpen: {
+    transform: 'translateX(0px)'
   },
   shadow: {
     position: 'absolute',
@@ -23,12 +29,25 @@ const useStyles = makeStyles({
 })
 
 export const UserSettings:FunctionComponent = () => {
-  const classes = useStyles();
+  const [isOpenSettings, setIsOpenSettings] = useState(false);
+
+  const classes = useStyles()
+
+  useEffect(
+    () => {setInterval(() => setIsOpenSettings(i => !i),1000)},
+    []
+  )
 
   return (
     <>
-      <div className={classes.wrapSettings}>settings</div>
-      <div className={classes.shadow}>1</div>
+      <div className={cn(
+          classes.settings,
+        {[classes.settingsOpen]: isOpenSettings}
+        )}
+      >
+        settings
+      </div>
+      <div className={classes.shadow}></div>
     </>
   )
 }
